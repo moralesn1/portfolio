02356e98625f972af1291191a202ef96c57f4e6d
Nathaniel <nathanielmorales01@gmail.com>
2021-04-29 23:21:13 +1000

    css

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,34 +2,34 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 export default function ProjectCard(props) {
-  return (
-    <>
-      <Card className="project-cards" style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={props.image} />
-        <Card.Body>
-          <Card.Title>{props.title}</Card.Title>
-          <Card.Subtitle>{props.tstack}</Card.Subtitle>
-          <Card.Text>{props.description}</Card.Text>
-          <a
-            className="projectcard-anchor-link"
-            href={props.repo}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Repo
-          </a>
-          {props.link ? (
-            <a
-              className="projectcard-anchor-link"
-              href={props.link}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vist the App
-            </a>
-          ) : null}
-        </Card.Body>
-      </Card>
-    </>
-  );
+	return (
+		<>
+			<Card className="project-cards" style={{ width: "18rem" }}>
+				<Card.Img variant="top" src={props.image} />
+				<Card.Body>
+					<Card.Title>{props.title}</Card.Title>
+					<Card.Subtitle>{props.tstack}</Card.Subtitle>
+					<Card.Text>{props.description}</Card.Text>
+					<a
+						className="projectcard-anchor-link"
+						href={props.repo}
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Repo
+					</a>
+					{props.link ? (
+						<a
+							className="projectcard-anchor-link"
+							href={props.link}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Vist the App
+						</a>
+					) : null}
+				</Card.Body>
+			</Card>
+		</>
+	);
 }
